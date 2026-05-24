a47fb17eb7e68290c55086dfd2302b713668c043
luckkim123 <luckkim123@gmail.com>
2026-05-25 02:43:27 +0900

    feat: package metadata + self-registration on marinelab

---
+++ constrained_albc/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (c) 2026.
+"""constrained-albc: full-DOF ALBC research environments and algorithms."""
+from .envs import constrained_full_albc  # noqa: F401  triggers gym.register()
+from .envs import constrained_full_albc_tdc  # noqa: F401
